1d4897a260b8e0a26122f3e4545eb810315648f7
Stark Wolf <cryptostarkwolf@gmail.com>
2020-10-28 16:54:37 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.aebdec4352524fcc996962b83fe169d6.js"
+  "/precache-manifest.2c411f152d96a13e757becbdf8ada731.js"
 );
 
 self.addEventListener('message', (event) => {
