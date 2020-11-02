11567d98bb27c733e856565e98be72bdebe31bb8
Stark Wolf <cryptostarkwolf@gmail.com>
2020-11-01 18:02:29 -0800

    Updates

---
+++ precache-manifest.b05f338fe2c59412053cf7db3353c12d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31056bd7e5b028889c718d87475811c4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9b7c4367b86431157fa4",
+    "url": "./static/css/2.b2f03837.chunk.css"
+  },
+  {
+    "revision": "4ae58a2ccd897e3a7c2c",
+    "url": "./static/css/main.d19b4672.chunk.css"
+  },
+  {
+    "revision": "9b7c4367b86431157fa4",
+    "url": "./static/js/2.fdfc92a1.chunk.js"
+  },
+  {
+    "revision": "ed364e94a6f6b1bb953368935d752ffd",
+    "url": "./static/js/2.fdfc92a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ae58a2ccd897e3a7c2c",
+    "url": "./static/js/main.65c58992.chunk.js"
+  },
+  {
+    "revision": "8049fe0a1a69c67a7ca5",
+    "url": "./static/js/runtime-main.17bdf1eb.js"
+  },
+  {
+    "revision": "49174c736a9c871baaeb2db6d5372eee",
+    "url": "./static/media/logo.49174c73.svg"
+  }
+]);
